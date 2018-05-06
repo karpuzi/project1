a374e3ff37d6c36fdcdf2ea05d89d271e102d21c
karpuzi <33609793+karpuzi@users.noreply.github.com>
2018-05-06 15:51:21 +0200

    initializing

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    //toggle btn
+    $('.navbar-toggler').click(function(){
+        $('.navbar-toggler').toggleClass('change')
+        
+    })
+    //sticky navbar
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position>=700){
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        }
+        else{
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    
+    //smooth scroll
+    $('.nav-item a, #back-to-top').click(function(link){
+        link.preventDefault();
+        let target = $(this).attr('href');
+        $('html,body').stop().animate({
+            scrollTop: $(target).offset().top - 25
+        },2000);
+    })
+    //back to top
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position>=700){
+            $('#back-to-top').addClass('scrollTop');
+        }
+        else{
+            $('#back-to-top').removeClass('scrollTop');
+        }
+    })
+    
+    //MAGNIGIC POPUP  
+    $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+        enabled:true
+    }
+    // other options
+    });
+    
+    
+});
